a01cc41abe70063b54b58a8b319fb998c0ca058c
Ana Fdez <afminguela@gmail.com>
2024-04-30 11:08:29 +0200

    testing hecho

---
+++ ___test___/fizzbuzz.test.js
@@ -0,0 +1,26 @@
+import { fizzbuzz } from "../src/fizzbuzz";
+
+test("debe testear que devuelva 'Fizz' si introduzco un 3", () => {
+    let result = fizzbuzz(3);
+    expect(result).toBe("Fizz");
+})
+
+test("debe testear que devuelva 'Buzz' si intrduzco un 5", () => {
+    let result = fizzbuzz(5);
+    expect(result).toBe("Buzz");
+})
+
+test("debe testear que devuelva 'Fizz' si introduzco un 18", () => {
+    let result = fizzbuzz(18);
+    expect(result).toBe("Fizz");
+})
+
+test("debe testear que devuelva 'Buzz' si introduzco un 50", () => {
+    let result = fizzbuzz(50);
+    expect(result).toBe("Buzz");
+})
+
+test("debe testear que devuelva 'FizzBuzz' si introduzco un 15", () => {
+    let result = fizzbuzz(15);
+    expect(result).toBe("FizzBuzz");
+})
---
+++ src/fizzbuzz.js
@@ -0,0 +1,12 @@
+const fizzbuzz = (number) => {
+if (typeof number === "number"){
+   if (number === 15){
+    return "FizzBuzz";
+   } else if (number%5 == 0){
+    return "Buzz";
+   } else if(number%3 == 0){
+    return "Fizz";
+   }
+ }}
+ 
+ export { fizzbuzz };
